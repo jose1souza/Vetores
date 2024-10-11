65e0d9998d2c0783ed2453557ee9784b2fde2000
José Carlos Souza <166337638+jose1souza@users.noreply.github.com>
2024-10-11 16:03:06 -0300

    Add files via upload

---
+++ MaiorVetor.cs
@@ -0,0 +1,31 @@
+﻿
+using Arrays;
+using System;
+
+class MaiorVetor
+{ 
+    static double MVetor(double[] vetor)
+    {   // percorrer o vetor e mostrar o maior
+        double maior = vetor[0];
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            if (vetor[i] > maior)
+            {
+                maior = vetor[i];
+            }
+        }
+        return maior;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Tamanho do vetor: ");
+        int n = int.Parse(Console.ReadLine());
+        double[] meuVetor = new double[n];
+        BibliotecaArray.GeraVetor(meuVetor);
+        Console.WriteLine("Vetor gerado: ");
+        BibliotecaArray.MostraVetor(meuVetor);
+        Console.WriteLine($"\n O maior número do vetor é igual a: {MVetor(meuVetor):F2}");
+        Console.ReadKey();
+
+    }
+}
---
+++ SomaVetores.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Arrays;
+class SomaVetores
+{
+    static int SomaVetor(int[] vetor)
+    {   // percorrer o vetor e somar. retorne a soma
+        int soma = 0;
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            soma = soma + vetor[i];
+        }
+        return soma;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Tamanho do vetor: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] meuVetor = new int[n];
+        BibliotecaArray.GeraVetor(meuVetor);
+        Console.WriteLine("Vetor gerado: ");
+        BibliotecaArray.MostraVetor(meuVetor);
+        Console.WriteLine($"\n A soma do vetor é igual a: {SomaVetor(meuVetor)}");
+        Console.ReadKey();
+
+    }
+}
