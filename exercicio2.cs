5127936ac6be3c8ee80efc7e210455d7159333e6
José Carlos Souza <166337638+jose1souza@users.noreply.github.com>
2024-10-11 09:10:21 -0300

    Add files via upload

---
+++ exercicio2.cs
@@ -0,0 +1,30 @@
+﻿using Arrays;
+using System;
+
+class exercicio2
+{
+    static double maiorVetor(double[] vetor)
+    {   // percorrer o vetor e mostrar o maior
+        double maior = vetor[0];
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            if (vetor[i] > maior)
+            {
+                maior = vetor[i];
+            }
+        }
+        return maior;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Tamanho do vetor: ");
+        int n = int.Parse(Console.ReadLine());
+        double[] meuVetor = new double[n];
+        BibliotecaArray.geraVetor(meuVetor);
+        Console.WriteLine("Vetor gerado: ");
+        BibliotecaArray.mostraVetor(meuVetor);
+        Console.WriteLine($"\n O maior número do vetor é igual a: {maiorVetor(meuVetor):F2}");
+        Console.ReadKey();
+
+    }
+}
---
+++ exercicio3.cs
@@ -0,0 +1,30 @@
+﻿using Arrays;
+using System;
+
+class exercicio3
+{
+    static double menorVetor(double[] vetor)
+    {   // percorrer o vetor e mostrar o menor
+        double menor = vetor[0];
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            if (vetor[i] < menor)
+            {
+                menor = vetor[i];
+            }
+        }
+        return menor;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Tamanho do vetor: ");
+        int n = int.Parse(Console.ReadLine());
+        double[] meuVetor = new double[n];
+        BibliotecaArray.geraVetor(meuVetor);
+        Console.WriteLine("Vetor gerado: ");
+        BibliotecaArray.mostraVetor(meuVetor);
+        Console.WriteLine($"\n O menor número do vetor é igual a: {menorVetor(meuVetor):F2}");
+        Console.ReadKey();
+
+    }
+}
