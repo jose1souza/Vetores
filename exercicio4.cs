30bc7d81d0d4f3d6f2daee72b57894ff249a2155
José Carlos Souza <166337638+jose1souza@users.noreply.github.com>
2024-10-11 09:45:37 -0300

    Add files via upload

---
+++ exercicio4.cs
@@ -0,0 +1,31 @@
+﻿using Arrays;
+using System;
+
+public class exercicio4
+{
+    static int Nimpar(int[] vetor)
+    {   // percorrer o vetor e mostrar o menor
+        int cont;
+        cont = 0;
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            if (vetor[i] % 2 != 0)
+            {
+                cont++;
+            }
+    
+        }
+        return cont;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Escolha o tamanho do vetor: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] meuVetor = new int[n];
+        BibliotecaArray.geraVetor(meuVetor);
+        Console.WriteLine("O vetor gerado é: ");
+        BibliotecaArray.mostraVetor(meuVetor);
+        Console.WriteLine($"\n A quantidade de números ímpares no vetor é: {Nimpar(meuVetor)}");
+        Console.ReadKey();
+
+    }
